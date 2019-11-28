f6e7118262f973810fde59bfeae849576a77fc0b
wpc <1007991483@qq.com>
2019-11-28 19:28:48 +0800

    somechange

--- meitu/src/main/java/edu/tjrac/swant/meitu/adapter/TabListAdapter.kt
+++ meitu/src/main/java/edu/tjrac/swant/meitu/adapter/TabListAdapter.kt
@@ -1,7 +1,9 @@
 package edu.tjrac.swant.meitu.adapter
 
+import android.widget.CheckBox
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
+import edu.tjrac.swant.meitu.R
 import edu.tjrac.swant.meitu.bean.Tab
 
 /**
@@ -10,6 +12,8 @@ import edu.tjrac.swant.meitu.bean.Tab
 class TabListAdapter(layoutResId: Int, data: List<Tab>?) : BaseQuickAdapter<Tab, BaseViewHolder>(layoutResId, data) {
 
     override fun convert(helper: BaseViewHolder, item: Tab) {
+        var cb = helper.getView<CheckBox>(R.id.cb)
+        cb.text = item.name
 
     }
 }
