3556d94d09f66c0a16ded9d577a7b28fc76feb7d
wpc <1007991483@qq.com>
2019-12-03 08:47:12 +0800

    Update Config.kt

--- meitu/src/main/java/edu/tjrac/swant/meitu/Config.kt
+++ meitu/src/main/java/edu/tjrac/swant/meitu/Config.kt
@@ -7,21 +7,20 @@ class Config{
     open class URL {
         companion object {
             val HTTP = false
-            val HOST = "122.51.205.29"//release
-//            val HOST = "10.10.29.249"//work
+//            val HOST = "122.51.205.29"//release
+            val HOST = "10.10.29.249"//work
 //          val HOST = "192.168.0.100"//home
-
             val API_SERVER = if(HTTP){"https://"}else{"http://"}+HOST + ":8080"
             var FILE_SERVER = if(HTTP){"https://"}else{"http://"}+HOST + ":8081"
-
             var MODEL_DIR = FILE_SERVER + "/t/"
         }
     }
 
     open class SP{
         companion object{
-            val FIRST_TIME = "first_time"
             val CACHE = "CACHE"
+
+            val FIRST_TIME = "first_time"
             val TOKEN="token"
 
             val ISNIGHT_MODE = "is_night_mode"
