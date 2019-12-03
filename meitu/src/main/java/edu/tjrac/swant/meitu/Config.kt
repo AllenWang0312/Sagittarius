05d22290360b779c43de6b333c2fda7c56e943ad
wpc <1007991483@qq.com>
2019-12-03 09:42:29 +0800

    somechange

--- meitu/src/main/java/edu/tjrac/swant/meitu/Config.kt
+++ meitu/src/main/java/edu/tjrac/swant/meitu/Config.kt
@@ -3,26 +3,34 @@ package edu.tjrac.swant.meitu
 /**
  * Created by wpc on 2019-11-20.
  */
-class Config{
+class Config {
     open class URL {
         companion object {
             val HTTP = false
 //            val HOST = "122.51.205.29"//release
 //            val HOST = "10.10.29.249"//work
-          val HOST = "192.168.0.102"//home
 
-            val API_SERVER = if(HTTP){"https://"}else{"http://"}+HOST + ":8080"
-            var FILE_SERVER = if(HTTP){"https://"}else{"http://"}+HOST + ":8081"
+            val HOST = "192.168.0.102"//home
+            val API_SERVER = if (HTTP) {
+                "https://"
+            } else {
+                "http://"
+            } + HOST + ":8080"
+            var FILE_SERVER = if (HTTP) {
+                "https://"
+            } else {
+                "http://"
+            } + HOST + ":8081"
 
             var MODEL_DIR = FILE_SERVER + "/t/"
         }
     }
 
-    open class SP{
-        companion object{
+    open class SP {
+        companion object {
             val FIRST_TIME = "first_time"
             val CACHE = "CACHE"
-            val TOKEN="token"
+            val TOKEN = "token"
 
             val ISNIGHT_MODE = "is_night_mode"
             val LANGUAGE_FOLLOW_SYSTEM = "follow_system"
